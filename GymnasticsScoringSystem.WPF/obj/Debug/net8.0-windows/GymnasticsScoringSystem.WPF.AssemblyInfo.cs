6dedc7baa5c74570fafdc9a328aefeaae76d3223
karton1tv <nekarton1tv@gmail.com>
2026-05-02 11:28:31 +0300

    Исправлен CI/CD5

--- GymnasticsScoringSystem.WPF/obj/Debug/net8.0-windows/GymnasticsScoringSystem.WPF.AssemblyInfo.cs
+++ GymnasticsScoringSystem.WPF/obj/Debug/net8.0-windows/GymnasticsScoringSystem.WPF.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GymnasticsScoringSystem.WPF")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8870e145b882adfa101dd03f2a1ad471e84bf0b1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54acb64cfdd0d3e0c83edc3ffa286e271b00fede")]
 [assembly: System.Reflection.AssemblyProductAttribute("GymnasticsScoringSystem.WPF")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GymnasticsScoringSystem.WPF")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
